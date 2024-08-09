6bc5364ae2d39abddda772681d08d1152767ef80
Mdelvalle <migdelv@gmail.com>
2024-08-09 17:23:47 -0500

    Force dynamic page for analytics

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { BsArrowLeft } from "react-icons/bs";
 import utils from "../utils";
 
+export const dynamic = "force-dynamic";
+
 export default async function page() {
   const mostPopular = await fetchMostPopular();
   const mostActiveTimes = await fetchMostActiveTimes();
